d753b08f8808272cbe6bac969c59ae3db80c3348
jakescode <jakestr1999@gmail.com>
2016-05-06 12:21:17 +0100

    RedditSharper

--- RedditSharper/Form1.cs
+++ RedditSharper/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net.Http;
+using System.Web;
 
 namespace RedditSharper
 {
@@ -15,6 +17,25 @@ namespace RedditSharper
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("https://www.reddit.com/api/v1/authorize?client_id=wJOPK4JHjb316Q&response_type=code&state=jakeisadeveloper&redirect_uri=https://www.reddit.com/&duration=permanent&scope=identity");
+            
+        }
+
+        private void webBrowser1_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            if(webBrowser1.Url.ToString().Substring(0, 25) != "https://www.reddit.com/lo")
+            {
+                string code = HttpUtility.ParseQueryString(webBrowser1.Url.Query).Get("code");
+                if(HttpUtility.ParseQueryString(webBrowser1.Url.Query).Get("state") == "jakeisadeveloper")
+                {
+                    // Post Request //
+                    // Guide here: https://github.com/reddit/reddit/wiki/OAuth2 //
+                }
+            }
         }
     }
 }
